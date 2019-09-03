50bb65357566e1478e602c6497f917f1e546a6b4
dehezhang2 <dehezhang2@gmail.com>
2019-09-03 11:52:38 +0800

    add local storage

--- component/FavoritePage.component.js
+++ component/FavoritePage.component.js
@@ -1,7 +1,6 @@
 import React , {Component} from 'react';
-import { StyleSheet, Image, ScrollView, ActivityIndicator, FlatList } from 'react-native';
-import { Container, Header, Item, Input, Card, CardItem, Text, Button, Icon, Left, Right } from 'native-base';
-import axios from 'axios';
+import { StyleSheet, Image, ScrollView, FlatList, View,SafeAreaView } from 'react-native';
+import { Container, Card, CardItem, Text, Button, Icon, Left, Right, Header } from 'native-base';
 import AsyncStorage from '@react-native-community/async-storage';
 export default class FavoritePage extends Component {
     constructor(props) {
@@ -9,6 +8,7 @@ export default class FavoritePage extends Component {
         this.state = {
             gifs: [],
         };
+        this.deleteItem = this.deleteItem.bind(this);
     }
     _loadFavoriteList = async()=>{
         const list = await AsyncStorage.getItem('favorite');
@@ -19,67 +19,117 @@ export default class FavoritePage extends Component {
                 gifs: JSON.parse(list)
             })
         }
+        this.onRefresh = this.onRefresh.bind(this);
+    }
+    deleteItem = (e)=>{
+        var array = [...this.state.gifs];
+        var index = array.indexOf(e);
+        if (index !== -1) {
+            array.splice(index, 1);
+            this.setState({ gifs: array }, () => {
+                AsyncStorage.setItem('favorite', JSON.stringify(this.state.gifs));
+            });
+        }
+    }
+    onRefresh(){
+        this._loadFavoriteList();
     }
     componentDidMount(){
         this._loadFavoriteList();
     }
     render(){
-        return (
-            <Container>
-                <ScrollView>
-                    <Container>
-                        <FlatList
-                            data={this.state.gifs}
-                            renderItem={({ item, index }) => (
+        if (this.state.gifs.length == 0) {
+            return (
+                <View style={styles.containerText}>
+                    <SafeAreaView />
+                    <Text style={styles.emptyText}>Favorite GIF List Empty</Text>
+                </View>
+            )
 
-                                <Card>
-                                    <CardItem>
-                                        <Text>{item.title}</Text>
-                                    </CardItem>   
-                                    <CardItem cardBody>
-                                        <Image source={{uri: item.key}} style={{height: 200, width: null, flex: 1}}/>
-                                    </CardItem>   
-                                    <CardItem>
-                                        <Left>
-                                        <Button transparent>
-                                            <Icon active name="ios-heart-empty" />
-                                        </Button>
-                                        </Left>
-                                        <Right>
-                                            <Text style={{color:'blue'}}>
-                                                {item.date}
-                                            </Text>
-                                        </Right>
-                                    </CardItem>
-                                </Card> 
+        } else {
+            return (
+                <Container>
+                    <Header>
+                        <Text style={styles.emptyText}>My Favorite GIF List</Text>
+                        <Button 
+                            transparent
+                            onPress = {this.onRefresh()}
+                        >
+                            <Text>Refresh</Text>
+                        </Button>
+                    </Header>
 
-                            )}
-                        />
-                    </Container>
-                </ScrollView>
-            </Container>
-        );
+                    <ScrollView>
+                        <Container>
+                            <FlatList
+                                data={this.state.gifs}
+                                renderItem={({ item, index }) => (      
+                                    <Card>
+                                        <CardItem>
+                                            <Text>{item.title}</Text>
+                                        </CardItem>   
+                                        <CardItem cardBody>
+                                            <Image source={{uri: item.key}} style={{height: 200, width: null, flex: 1}}/>
+                                        </CardItem>   
+                                        <CardItem>
+                                            <Left>
+                                            <Button 
+                                                transparent
+                                                onPress = {()=>{this.deleteItem(item)}}
+                                            >
+                                                <Icon active name="ios-trash" />
+                                            </Button>
+                                            </Left>
+                                            <Right>
+                                                <Text style={{color:'blue'}}>
+                                                    {item.date}
+                                                </Text>
+                                            </Right>
+                                        </CardItem>
+                                    </Card>         
+                                )}
+                            />
+                        </Container>
+                    </ScrollView>
+                </Container>
+            );
+        }
     }
 }
 const styles = StyleSheet.create({
-  itemContainer: {
-    justifyContent: 'flex-end',
-    borderRadius: 5,
-    padding: 10,
-    height: 150,
-  },
-  itemName: {
-    fontSize: 16,
-    color: '#fff',
-    fontWeight: '600',
-  },
-  itemCode: {
-    fontWeight: '600',
-    fontSize: 12,
-    color: '#fff',
-  },
-  loader: {
-      marginTop: 10,
-      alignItems: 'center'
-  },
+    itemContainer: {
+        justifyContent: 'flex-end',
+        borderRadius: 5,
+        padding: 10,
+        height: 150,
+    },
+    itemName: {
+        fontSize: 16,
+        color: '#fff',
+        fontWeight: '600',
+    },
+    itemCode: {
+        fontWeight: '600',
+        fontSize: 12,
+        color: '#fff',
+    },
+    loader: {
+        marginTop: 10,
+        alignItems: 'center'
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    containerText: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    emptyText: {
+        justifyContent: 'center',
+        lineHeight: 30,
+        fontSize: 30
+    },
+
 });
